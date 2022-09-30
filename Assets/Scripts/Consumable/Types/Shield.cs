7d3087737a1debf1ca0cda2ac6c2d69d9572b195
주진우 <67905493+G-Nck@users.noreply.github.com>
2022-09-30 15:08:07 +0900

    Update Shield.cs

--- Assets/Scripts/Consumable/Types/Shield.cs
+++ Assets/Scripts/Consumable/Types/Shield.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class Shield : Consumable
 {
+    public AssetReference DestroyedParticleReference;
+
+
     public override string GetConsumableName()
     {
         return "Shield";
@@ -23,6 +28,55 @@ public class Shield : Consumable
     {
         return 500;
 
+    }
+
+
+    public override void Tick(CharacterInputController c)
+    {
+        base.Tick(c);
+
+    }
+
+    public override IEnumerator Started(CharacterInputController c)
+    {
+        yield return base.Started(c);
+        c.characterCollider.isShieldEnabled = true;
+        handler = () => OnTriggerObstacle(c);
+        c.characterCollider.OnTriggeredObstacle += handler;
+
+
+    }
+
+    public override void Ended(CharacterInputController c)
+    {
+        base.Ended(c);
+        c.characterCollider.OnTriggeredObstacle -= handler;
+
+        c.characterCollider.isShieldEnabled = false;
+    }
+
+    void OnTriggerObstacle(CharacterInputController c)
+    {
+
+        m_SinceStart += duration;
+
+        c.StartCoroutine(SpawnDestroyedVFX(c));
+    }
+
+    Action handler;
+
+    IEnumerator SpawnDestroyedVFX(CharacterInputController c)
+    {
+        if (DestroyedParticleReference != null)
+        {
+            //Addressables 1.0.1-preview
+            var op = DestroyedParticleReference.InstantiateAsync();
+            yield return op;
+            m_ParticleSpawned = op.Result.GetComponent<ParticleSystem>();
+
+            m_ParticleSpawned.transform.SetParent(c.characterCollider.transform);
+            m_ParticleSpawned.transform.localPosition = op.Result.transform.position;
+        }
     }
 
 }
